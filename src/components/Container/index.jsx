84cfd87065ab3b6a7a45187c8e0bce30c4f4ad2c
markschwenck <markcell.1@gmail.com>
2024-02-14 23:05:56 -0300

    NavBar

---
+++ src/components/Container/index.jsx
@@ -0,0 +1,12 @@
+import styles from './Container.module.css'
+
+function Container () {
+    return (
+        <Container className= {styles.Container}>
+
+        </Container>
+
+    )
+}
+
+export default Container

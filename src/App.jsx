93e144776b4c95fde1a021021aba301574dbe3cc
markschwenck <markcell.1@gmail.com>
2024-02-18 18:53:03 -0300

    Estruturação do Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   
@@ -8,6 +9,8 @@ function App() {
   return (
     <>
       <Header/>
+
+      <Footer/>
     </>
   )
 }
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,28 @@
 import styles from '../Footer/Footer.module.css'
+import Logo from '../../assets/Logo/Logo-reformulada.png'
 
 function Footer() {
     return (
-        <footer>
+        <footer className={styles.footer}>
+            <div>
+            <span>
+               <img src= {Logo} alt="" />
+            </span>
+                <p>Direitos</p>
+            </div>
+
+            <div>
+                <ul>
+                    <li href="">Home</li>
+                    <li href="">Sobre</li>
+                    <li href="">Projetos</li>
+                    <li href="">Contato</li>
+                </ul>
+            </div>
+
+            <div>
+                Localização e contato
+            </div>
             
         </footer>
     )
